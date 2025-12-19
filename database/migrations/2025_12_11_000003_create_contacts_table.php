dc5cfaafc7d9be0acf9b6a1e66e3cb1caf322539
Pritam Bag <pritam02b@gmail.com>
2025-12-19 22:17:13 +0530

    fix: naming bugs

--- database/migrations/2025_12_11_000003_create_contacts_table.php
+++ database/migrations/2025_12_11_000003_create_contacts_table.php
@@ -22,14 +22,18 @@ return new class extends Migration
         // New contact_metas table
         Schema::create('contact_metas', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('ref_parent')->constrained()->onDelete('cascade');
+
+            $table->foreignId('ref_parent')
+                ->constrained('contacts')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
             $table->string('meta_key');
             $table->longText('meta_value');
             $table->string('status')->default('active');
             $table->bigInteger('created_by')->default(0);
             $table->bigInteger('updated_by')->default(0);
             $table->timestamps();
-            $table->foreign('ref_parent')->references('id')->on('contacts')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
