7289a8867473808184f05049c71a5ce12b12c8dd
Pritam Bag <pritam02b@gmail.com>
2025-12-29 11:21:16 +0530

    enhancement: added api configuration

--- src/Config/SmartMessengerConf.php
+++ src/Config/SmartMessengerConf.php
@@ -3,6 +3,7 @@
 namespace Iquesters\SmartMessenger\Config;
 
 use Iquesters\Foundation\Support\BaseConf;
+use Iquesters\Foundation\Support\ApiConf;
 use Iquesters\Foundation\Enums\Module;
 
 class SmartMessengerConf extends BaseConf
@@ -10,9 +11,13 @@ class SmartMessengerConf extends BaseConf
     // Inherited property of BaseConf, must initialize
     protected ?string $identifier = Module::SMART_MESSENGER;
     
-    
+    // properties of this class
+    protected ApiConf $api_conf;
+
     protected function prepareDefault(BaseConf $default_values)
     {
-        
+        $default_values->api_conf = new ApiConf();
+        $default_values->api_conf->prefix = 'smart-messenger'; // Must be auto generated from module enum - the vendor name  
+        $default_values->api_conf->prepareDefault($default_values->api_conf);
     }
 }
