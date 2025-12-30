ce53bb01fe447e5b6d08165dc200e6f0774d3468
Pritam Bag <pritam02b@gmail.com>
2025-12-30 21:07:45 +0530

    test: remove code for testing api

--- src/Config/SmartMessengerConf.php
+++ src/Config/SmartMessengerConf.php
@@ -12,12 +12,12 @@ class SmartMessengerConf extends BaseConf
     protected ?string $identifier = Module::SMART_MESSENGER;
     
     // properties of this class
-    protected ApiConf $api_conf;
+    // protected ApiConf $api_conf;
 
     protected function prepareDefault(BaseConf $default_values)
     {
-        $default_values->api_conf = new ApiConf();
-        $default_values->api_conf->prefix = 'smart-messenger'; // Must be auto generated from module enum - the vendor name  
-        $default_values->api_conf->prepareDefault($default_values->api_conf);
+        // $default_values->api_conf = new ApiConf();
+        // $default_values->api_conf->prefix = 'smart-messenger'; // Must be auto generated from module enum - the vendor name  
+        // $default_values->api_conf->prepareDefault($default_values->api_conf);
     }
 }
