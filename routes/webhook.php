158f0d4dda925e26779c0c08448635321c714862
Pritam Bag <pritam02b@gmail.com>
2026-01-09 13:21:29 +0530

    update: webhook url and validation

--- routes/webhook.php
+++ routes/webhook.php
@@ -3,5 +3,5 @@
 use Iquesters\SmartMessenger\Http\Controllers\Webhook\WhatsAppWHController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/webhook/whatsapp', [WhatsAppWHController::class, 'handle']);
-Route::get('/webhook/whatsapp', [WhatsAppWHController::class, 'handle']);
+Route::post('/webhook/whatsapp/{channelUid}', [WhatsAppWHController::class, 'handle']);
+Route::get('/webhook/whatsapp/{channelUid}', [WhatsAppWHController::class, 'handle']);
