4853e4a0eac04d8a3f6e07d36a595773950fb1af
Pritam Bag <pritam02b@gmail.com>
2026-02-25 00:15:57 +0530

    enhancement: add diagnostics feature with API integration and UI updates for feedback and dev mode

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Iquesters\SmartMessenger\Http\Controllers\Api\ContactController;
+use Iquesters\SmartMessenger\Http\Controllers\Api\DiagnosticsController;
 
 // All middleware and prefix are handled in the service provider
 Route::get('/contacts', [ContactController::class, 'index']);
 Route::post('/contacts', [ContactController::class, 'store']);
 Route::put('/contacts/{uid}', [ContactController::class, 'update']);
+Route::get('/diagnostics/{integrationId}/message/{messageId}', [DiagnosticsController::class, 'show']);
